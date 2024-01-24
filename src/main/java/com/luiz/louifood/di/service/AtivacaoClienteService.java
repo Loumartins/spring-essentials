126dd99253bb698bf85cb476659e4191c999151e
Luiz <luizhmartins11@gmail.com>
2024-01-24 09:13:59 -0300

    identificando outros pontos de DI.

--- src/main/java/com/luiz/louifood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/luiz/louifood/di/service/AtivacaoClienteService.java
@@ -3,19 +3,29 @@ package com.luiz.louifood.di.service;
 import com.luiz.louifood.di.modelo.Cliente;
 import com.luiz.louifood.di.notificacao.Notificador;
 import com.luiz.louifood.di.notificacao.NotificadorEmail;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
+@Component
 public class AtivacaoClienteService {
+    //injecao de dependecia no atributo
+    @Autowired
     private Notificador notificador;
 
-    public AtivacaoClienteService(Notificador notificador) {
-        this.notificador = notificador;
-        System.out.println("AtivacaoClienteService: " + notificador);
-    }
+//    ponto de injeçao de dependencia no constructor
+//    @Autowired
+//    public AtivacaoClienteService(Notificador notificador) {
+//        this.notificador = notificador;
+//    }
+
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
         notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
     }
+//    ponto de injecao de dependencia no setter
+//    @Autowired
+//    public void setNotificador(Notificador notificador) {
+//        this.notificador = notificador;
+//    }
 }
